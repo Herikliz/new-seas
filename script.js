ed080eb357841a56c4a515b1a7d597f28c67e576
Herikliz <ericleseduardo353@gmail.com>
2026-04-07 02:08:37 -0300

    Estilo

--- script.js
+++ script.js
@@ -2197,7 +2197,7 @@ function updateUI() {
         });
 
         estilosKeys.forEach(stKey => {
-            tecnicasOut += `« ${stKey} »\n\n`;
+            tecnicasOut += `« ${stKey} »\n`;
             agrupado[stKey].forEach(t => {
                 if (t.nome) tecnicasOut += `* ${t.nome}\n`;
                 if (t.desc) { t.desc.split('\n').forEach(line => { let trimLine = line.trim(); if(trimLine !== "") tecnicasOut += `> ${trimLine.replace(/^>\s*/, '')}\n`; }); }
